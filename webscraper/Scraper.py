73e0e3fcd0edceb6ea698be1b1d78a9aee701755
Irvin Shen <irvinshen.texas@gmail.com>
2022-04-24 04:45:10 -0500

    Updated some scraping bugs and added comments

--- webscraper/Scraper.py
+++ webscraper/Scraper.py
@@ -6,7 +6,25 @@ from typing import Union, List
 
 class Scraper:
     def __init__(self, category: Union[str, List[str]]):
-        self.category = category
+        if isinstance(category, str):
+            self.category = [category, ]
+        else:
+            self.category = category
+
+    @staticmethod
+    def generate_dataframe(soup: BeautifulSoup, player_id: str):
+        # We found the hidden table! Read in as a DataFrame
+        df = pd.read_html(str(soup))[0]
+        # Add unique bbref ID
+        df["bbrefID"] = player_id
+
+        # remove garbage "years" and rename to match the given data
+        df = df.rename({"Year": "yearID"}, axis=1)
+        df["yearID"] = pd.to_numeric(df["yearID"], errors="coerce", downcast="integer")
+        df = df.dropna(subset="yearID")
+        df.loc[:, "yearID"] = df.loc[:, "yearID"].astype("int16")
+
+        return df
 
     def parse_data(self, player_id: str) -> List[pd.DataFrame]:
         """
@@ -22,22 +40,27 @@ class Scraper:
         # Because we can have multiple categories, output needs to be a list
         output = []
         for label in self.category:
-            advanced_df = None
-            for hidden_content in page_soup.find_all(text=lambda text: isinstance(text, Comment)):
-                # Parsing comments for the hidden table
-                hidden_soup = BeautifulSoup(hidden_content, "lxml")
-                table_soup = hidden_soup.find('table', {"id": label})
-                if table_soup:
-                    # We found the hidden table! Read in as a DataFrame
-                    advanced_df = pd.read_html(str(table_soup))[0]
-                    # Add unique bbref ID
-                    advanced_df["bbrefID"] = player_id
-
-                    # remove garbage "years" and rename to match the given data
-                    advanced_df = advanced_df.rename({"Year": "yearID"}, axis=1)
-                    advanced_df["yearID"] = pd.to_numeric(advanced_df["yearID"], errors="coerce", downcast="integer")
-                    advanced_df = advanced_df.dropna(subset="yearID")
-                    break
             # We want to make sure that we return `None` if we don't get any hits for a given table type
+            advanced_df = None
+            # Pray that the table isn't hidden away in a comment
+            table_soup = page_soup.find('table', {"id": label})
+            if table_soup:
+                advanced_df = self.generate_dataframe(table_soup, player_id)
+                output.append(advanced_df)
+                continue
+
+            # Accept the crushing reality that it is hidden and iterate through all comments
+            for hidden_content in page_soup.find_all(text=lambda text: isinstance(text, Comment)):
+                # Parsing each comment back into its own soup
+                hidden_soup = BeautifulSoup(hidden_content, "lxml")
+                try:
+                    # Break early if we find the table
+                    table_soup = hidden_soup.find_all('table', {"id": label})[0]
+                    advanced_df = self.generate_dataframe(table_soup, player_id)
+                    break
+                except IndexError:
+                    continue
+
             output.append(advanced_df)
+
         return output
--- webscraper/pitcher_scraper.py
+++ webscraper/pitcher_scraper.py
@@ -13,17 +13,18 @@ if __name__ == "__main__":
     webscraper = Scraper(["batting_value", "pitching_value"])
 
     # Read in player IDs
-    player_ids = pd.read_csv("batting_ids.csv").to_numpy().flatten()
+    player_ids = pd.read_csv("pitching_ids.csv").to_numpy().flatten()
 
     # Initialize multiprocessor workload, bypassing GIL
     with Pool(cpu_count() * 2) as thread_pool:
         # Output of parse_data is iterable, so we need to just grab the first item since
         # we're only grabbing batting data, then re-combine
-        output = thread_pool.map(webscraper.parse_data, player_ids)
-        batting = [x[0] for x in output]
-        pitching = [x[1] for x in output]
-        batting_df = pd.concat(batting, axis=0)
-        pitching_df = pd.concat(pitching, axis=0)
+        output = thread_pool.map(webscraper.parse_data, player_ids[:64])
+
+    batting = [x[0] for x in output]
+    pitching = [x[1] for x in output]
+    batting_df = pd.concat(batting, axis=0)
+    pitching_df = pd.concat(pitching, axis=0)
 
     # Export final statistics
     pitching_df.to_csv("../baseball_data/advanced/pitching_advanced.csv", index=False)
